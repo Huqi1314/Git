7fadbe6ca88abb65f45fdf15108616e0ca9f28dd
Huqi1314 <95727105+Huqi1314@users.noreply.github.com>
2022-09-14 10:03:56 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("胡琪加油!!!!");
+        System.out.println("加油进入大厂");
     }
 }
